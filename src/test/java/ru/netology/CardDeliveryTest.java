c1ec5c91ed2ee183957da3d90f7baf7599548076
Chebykina-Xenia <chebykina.xenia@gmail.com>
2022-01-24 11:49:58 +0300

    Доработка ДЗ Patterns Задание 1

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -4,11 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
-
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
@@ -16,39 +11,26 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
 
-    RegistrationByUserInfo userInfo = DataGenerator.Registration.generateByUser("ru");
-
     @BeforeEach
     public void setUp() {
         //открываем сайт
         open("http://localhost:9999/");
     }
 
-    @Test
-    void shouldPreventSendRequestMultipleTimes() {
-        System.out.println(userInfo.getCity());
-        System.out.println(userInfo.getDate());
-        System.out.println(userInfo.getName());
-        System.out.println(userInfo.getPhone());
-    }
-
     //заполняем все поля — успешное отправление формы
     @Test
     void shouldFormSentSuccessfully() {
-
-        String userPhone = userInfo.getPhone();
-
         //заполняем поле город
-        $("[class='input__control'][placeholder='Город']").setValue("Краснодар");
+        $("[class='input__control'][placeholder='Город']").setValue(DataGenerator.generateCity());
         $("[class='menu-item__control']").click();
         //заполняем поле дата
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
         $("[data-test-id='date'] input").click();
-        $("[class='input__control'][placeholder='Дата встречи']").setValue(userInfo.getDate());
+        $("[class='input__control'][placeholder='Дата встречи']").setValue(DataGenerator.generateDate(5));
         //заполняем поле имя и фамилия
-        $("[class='input__control'][name='name']").setValue("Чебыкина Ксения");
+        $("[class='input__control'][name='name']").setValue(DataGenerator.generateName("ru"));
         //заполняем поле телефон
-        $("[class='input__control'][name='phone']").setValue("+" + userPhone);
+        $("[class='input__control'][name='phone']").setValue("+7" + DataGenerator.generatePhone("ru"));
         //ставим галочку согласия
         $("[data-test-id='agreement']").click();
         //нажимаем кнопку забронировать
@@ -59,7 +41,7 @@ public class CardDeliveryTest {
         //заполняем поле дата
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
         $("[data-test-id='date'] input").click();
-        $("[class='input__control'][placeholder='Дата встречи']").setValue(userInfo.getDate());
+        $("[class='input__control'][placeholder='Дата встречи']").setValue(DataGenerator.generateDate(10));
         //нажимаем кнопку забронировать
         $(byText("Запланировать")).click();
         //проверка успешной отправки формы
--- src/test/java/ru/netology/DataGenerator.java
+++ src/test/java/ru/netology/DataGenerator.java
@@ -1,31 +1,55 @@
 package ru.netology;
 
-import com.github.javafaker.CreditCardType;
 import com.github.javafaker.Faker;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+import java.util.Random;
 
 public class DataGenerator {
 
-    private DataGenerator() {
-
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public class UserInfo {
+        private String city;
+        private String name;
+        private String phone;
     }
 
-    public static class Registration {
-        private Registration() {
+    private DataGenerator() {
+    }
 
-        }
+    //генерируем дату, shift — количество дней на которое сдвигаем
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 
-        public static RegistrationByUserInfo generateByUser(String locale) {
-            Faker faker = new Faker(new Locale(locale));
-            return new RegistrationByUserInfo(
-                    faker.address().cityName(),
-                    LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")),
-                    faker.name().name(),
-                    faker.phoneNumber().subscriberNumber(11)
-            );
-        }
+    //генерируем номер телефона
+    public static String generatePhone(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        return faker.phoneNumber().subscriberNumber(10);
+    }
+
+    //генерируем город
+    public static String generateCity() {
+        String[] city = {"Вологда", "Краснодар", "Архангельск", "Калининград", "Самара", "Владимир"};
+        int rnd = new Random().nextInt(city.length);
+        return city[rnd];
+    }
+
+    //генерируем имя
+    public static String generateName(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        String name = faker.name().nameWithMiddle();
+        //разделяем строку и получаем массив из фамилии, имени и отчества
+        String[] words = name.split(" ");
+        //соединяем только имя и отчество
+        String nameUser = words[0] + " " + words[1];
+        return nameUser;
     }
 }
